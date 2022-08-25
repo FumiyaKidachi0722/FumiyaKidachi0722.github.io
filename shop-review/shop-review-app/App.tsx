40df6aa066cd2420fc85f1f37b7414a1b6d9f629
FumiyaKidachi <fumiya.kidachi@gmail.com>
2022-08-25 13:23:59 +0900

    no message

--- shop-review/shop-review-app/App.tsx
+++ shop-review/shop-review-app/App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import * as firebase from 'firebase';
+import "firebase/firestore";
 import { StyleSheet, Text, View } from 'react-native';
 
+if(!firebase.apps.length){
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdvEQOMGnA3J5tD1HxMuVNr3Vto0BF0zw",
+    authDomain: "shop-review-8df06.firebaseapp.com",
+    projectId: "shop-review-8df06",
+    storageBucket: "shop-review-8df06.appspot.com",
+    messagingSenderId: "104299107525",
+    appId: "1:104299107525:web:950afe8cbbcf17fdb00cac",
+    measurementId: "G-35B03KRTEV"
+  }
+  firebase.initializeApp(firebaseConfig);
+}
+
+
 export default function App() {
+  useEffect(() => {
+    getFirebaseItems();
+  }, [])
+
+  const getFirebaseItems = async() => {
+    const snapshot = await firebase.firestore().collection("shops").get();
+    const shops = snapshot.docs.map(doc => doc.data());
+    console.log(shops);
+  };
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
