f97f565046e4f425a310c35670cef514645bef7a
FumiyaKidachi <fumiya.kidachi@gmail.com>
2022-08-28 09:57:01 +0900

    no message

---
+++ node-scraping/index.js
@@ -0,0 +1,29 @@
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+//ウェブスクレイパーをつくろう
+const URL = "https://search.rakuten.co.jp/search/mall/keyboard/";
+const data = [];
+
+axios(URL)
+.then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+    const $ = cheerio.load(htmlParser);
+
+    $(".searchresultitem", htmlParser).each(function() {
+        const title = $(this).find(".title").text();
+        const price = $(this).find(".important").text();
+        data.push({ title, price });
+        console.log(data);
+    });
+})
+.catch((err) => console.log(err));
+
+
+
+app.listen(PORT, console.log("server running!"));
